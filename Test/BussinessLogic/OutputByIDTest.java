6b148f69cd02f9cbded8ecfd6b4cb746299ddead
alexanderkutin <alexander.kutin@gmail.com>
2019-02-02 15:56:13 +0200

    First Commit

---
+++ Test/BussinessLogic/OutputByIDTest.java
@@ -0,0 +1,21 @@
+package BussinessLogic;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OutputByIDTest {
+
+    private OutputByID outputByID;
+
+
+    @Before
+    public void setUp() throws Exception {
+        outputByID = new OutputByID();
+    }
+
+    @Test
+    public void getProduct() {
+    }
+}
---
+++ Test/BussinessLogic/ProductGeneratorTest.java
@@ -0,0 +1,19 @@
+package BussinessLogic;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProductGeneratorTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void shouldReturnProduct() {
+
+
+    }
+}
